0da40374f4102a1dd037231ea7719937cb84d874
Gabby Delforge <gabrielle.delforge@gmail.com>
2024-10-06 16:23:59 -0400

    newspaper

---
+++ Scripts/Newspaper.bindings.cs
@@ -0,0 +1,58 @@
+using Godot;
+
+
+public partial class Newspaper : ColorRect {
+  public static Newspaper New() {
+    return GD.Load<PackedScene>("res://Scenes/Newspaper.tscn").Instantiate<Newspaper>();
+  }
+  public Newspaper() {
+    foreach (var @interface in GetType().GetInterfaces()) {
+      AddToGroup(@interface.Name);
+    }
+  }
+
+  public partial class NewspaperNodes {
+    Newspaper parent;
+
+    public NewspaperNodes(Newspaper parent) {
+      this.parent = parent;
+    }
+    private TextureRect? _Newspaper_;
+    public TextureRect Newspaper_ {
+      get => _Newspaper_ ??= parent.GetNode<TextureRect>("Newspaper");
+    }
+
+    private VBoxContainer? _Newspaper_VBoxContainer;
+    public VBoxContainer Newspaper_VBoxContainer {
+      get => _Newspaper_VBoxContainer ??= parent.GetNode<VBoxContainer>("Newspaper/VBoxContainer");
+    }
+
+    private Label? _Newspaper_VBoxContainer_DaysLeft;
+    public Label Newspaper_VBoxContainer_DaysLeft {
+      get => _Newspaper_VBoxContainer_DaysLeft ??= parent.GetNode<Label>("Newspaper/VBoxContainer/DaysLeft");
+    }
+
+    private VBoxContainer? _Newspaper_VBoxContainer_NewspaperContentContainer;
+    public VBoxContainer Newspaper_VBoxContainer_NewspaperContentContainer {
+      get => _Newspaper_VBoxContainer_NewspaperContentContainer ??= parent.GetNode<VBoxContainer>("Newspaper/VBoxContainer/NewspaperContentContainer");
+    }
+
+    private Button? _Newspaper_CloseButton;
+    public Button Newspaper_CloseButton {
+      get => _Newspaper_CloseButton ??= parent.GetNode<Button>("Newspaper/CloseButton");
+    }
+
+    private NewspaperEntry? _Newspaper_VBoxContainer_NewspaperContentContainer_NewspaperEntry;
+    public NewspaperEntry Newspaper_VBoxContainer_NewspaperContentContainer_NewspaperEntry {
+      get => _Newspaper_VBoxContainer_NewspaperContentContainer_NewspaperEntry ??= parent.GetNode<NewspaperEntry>("Newspaper/VBoxContainer/NewspaperContentContainer/NewspaperEntry");
+    }
+
+  }
+
+  public NewspaperNodes? _Nodes;
+  public NewspaperNodes Nodes {
+    get {
+      return _Nodes ??= new NewspaperNodes(this);
+    }
+  }
+}
