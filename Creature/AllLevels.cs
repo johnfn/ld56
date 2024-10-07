8e38e5c9053e4be19340de9e12e1c4bbf4a8a0f1
LunacyEcho <chaunceylo.13@gmail.com>
2024-10-07 18:16:02 -0400

    updated char descs, + four days of "levels"

--- Creature/AllLevels.cs
+++ Creature/AllLevels.cs
@@ -9,26 +9,155 @@ public static class AllLevels {
       new SpawnedCreature {
         Data = AllCreatures.Hazel,
         ReservationTime = Clock.GetTimeFromString("8:00 AM"),
+        GetDialog = () => AllDialog.Hazel,
+
         State = CreatureState.NotSpawnedYet,
         Instance = null,
         CurrentScreen = CurrentScreen.Interior,
         SelectedChair = null,
-        GetDialog = () => AllDialog.Hazel,
-      }
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Chip,
+        ReservationTime = Clock.GetTimeFromString("10:00 AM"),
+        GetDialog = () => AllDialog.Chip,
+
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Kero,
+        ReservationTime = Clock.GetTimeFromString("3:00 PM"),
+        GetDialog = () => AllDialog.Kero,
+
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Rufus,
+        ReservationTime = Clock.GetTimeFromString("5:00 PM"),
+        GetDialog = () => AllDialog.Rufus,
+
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
     ],
 
     // Day 2
     [
       new SpawnedCreature {
-        Data = AllCreatures.Hazel,
-        ReservationTime = Clock.GetTimeFromString("10:00 AM"),
+        Data = AllCreatures.Bonnie,
+        ReservationTime = Clock.GetTimeFromString("8:00 AM"),
+        GetDialog = () => AllDialog.Bonnie,
+        
         State = CreatureState.NotSpawnedYet,
         Instance = null,
         CurrentScreen = CurrentScreen.Interior,
         SelectedChair = null,
-        GetDialog = () => AllDialog.Hazel,
-      }
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Poe,
+        ReservationTime = Clock.GetTimeFromString("10:00 AM"),
+        GetDialog = () => AllDialog.Poe,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Tom,
+        ReservationTime = Clock.GetTimeFromString("10:00 AM"),
+        GetDialog = () => AllDialog.Tom,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
     ],
+
+    // Day 3
+    [
+      new SpawnedCreature {
+        Data = AllCreatures.Speedy,
+        ReservationTime = Clock.GetTimeFromString("8:00 AM"),
+        GetDialog = () => AllDialog.Speedy,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Lav,
+        ReservationTime = Clock.GetTimeFromString("10:00 AM"),
+        GetDialog = () => AllDialog.Lav,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Jerry,
+        ReservationTime = Clock.GetTimeFromString("12:01 PM"),
+        GetDialog = () => AllDialog.Jerry,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+    ]
+
+    // Day 4
+    [
+      new SpawnedCreature {
+        Data = AllCreatures.Pip,
+        ReservationTime = Clock.GetTimeFromString("8:00 AM"),
+        GetDialog = () => AllDialog.Speedy,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Emily,
+        ReservationTime = Clock.GetTimeFromString("10:00 AM"),
+        GetDialog = () => AllDialog.Lav,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+
+      new SpawnedCreature {
+        Data = AllCreatures.Jerry,
+        ReservationTime = Clock.GetTimeFromString("12:01 PM"),
+        GetDialog = () => AllDialog.Jerry,
+        
+        State = CreatureState.NotSpawnedYet,
+        Instance = null,
+        CurrentScreen = CurrentScreen.Interior,
+        SelectedChair = null,
+      },
+    ]
   ];
 
 }
