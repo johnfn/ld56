c753f7988caee5707da4cbe4738c773132674b1f
johnfn <johnfn@gmail.com>
2024-10-07 16:30:55 -0400

    add alllevels no comments

--- Creature/AllLevels.cs
+++ Creature/AllLevels.cs
@@ -29,59 +29,6 @@ public static class AllLevels {
         GetDialog = () => AllDialog.Hazel,
       }
     ],
-
-    // [
-    //   new SpawnedCreature {
-    //   Data = AllCreatures.MrPig,
-    //   ReservationTime = Clock.GetTimeFromString("8:00 AM"),
-    //   State = CreatureState.NotSpawnedYet,
-    //   Instance = null,
-    //   CurrentScreen = CurrentScreen.Interior,
-    //   SelectedChair = null,
-    //   GetDialog = () => NextDialog.For[AllCreatures.MrPig],
-    // },
-
-    // new SpawnedCreature {
-    //   Data = AllCreatures.MrMouse,
-    //   ReservationTime = Clock.GetTimeFromString("10:00 AM"),
-    //   State = CreatureState.NotSpawnedYet,
-    //   Instance = null,
-    //   CurrentScreen = CurrentScreen.Interior,
-    //   SelectedChair = null,
-    //   GetDialog = () => NextDialog.For[AllCreatures.MrMouse],
-    // },
-
-    // new SpawnedCreature {
-    //   Data = AllCreatures.MrPig,
-    //   ReservationTime = Clock.GetTimeFromString("10:00 AM"),
-    //   State = CreatureState.NotSpawnedYet,
-    //   Instance = null,
-    //   CurrentScreen = CurrentScreen.Interior,
-    //   SelectedChair = null,
-    //   GetDialog = () => NextDialog.For[AllCreatures.MrPig],
-    // },
-
-    // new SpawnedCreature {
-    //   Data = AllCreatures.MrMouse,
-    //   ReservationTime = Clock.GetTimeFromString("12:00 PM"),
-    //   State = CreatureState.NotSpawnedYet,
-    //   Instance = null,
-    //   CurrentScreen = CurrentScreen.Interior,
-    //   SelectedChair = null,
-    //     GetDialog = () => NextDialog.For[AllCreatures.MrMouse],
-    //   },
-
-    // new SpawnedCreature {
-    //   Data = AllCreatures.MrMouse,
-    //   ReservationTime = Clock.GetTimeFromString("2:00 PM"),
-    //   State = CreatureState.NotSpawnedYet,
-    //   Instance = null,
-    //   CurrentScreen = CurrentScreen.Interior,
-    //   SelectedChair = null,
-    //     GetDialog = () => NextDialog.For[AllCreatures.MrMouse],
-    //   },
-    // ]
-
   ];
 
 }
